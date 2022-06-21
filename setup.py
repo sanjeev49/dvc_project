1ccd3a18d57f3563e56cace5ca3692b3d9ac36b3
sanjeev kumar <sanjeevkr4983@gmail.com>
2022-06-21 11:30:37 +0530

    new changes

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "dvc_project"
+AUTHOR_USER_NAME = "sanjeev49"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
